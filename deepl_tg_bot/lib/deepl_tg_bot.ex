2f333aacf0744dcd215b8a4b6e56aad3915547c9
Daniel Tomas <danieltomas-sanchez-8@hotmail.com>
2022-05-06 11:33:42 +0200

    Add brand new  mix project

---
+++ deepl_tg_bot/lib/deepl_tg_bot.ex
@@ -0,0 +1,18 @@
+defmodule DeeplTgBot do
+  @moduledoc """
+  Documentation for `DeeplTgBot`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DeeplTgBot.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
